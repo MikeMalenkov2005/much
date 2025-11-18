99616b9f495008aa62b7250844d17f269ff61934
MikeMalenkov2005 <mike.malenkov@gmail.com>
2025-11-18 19:41:36 +0300

    Added types.h file

---
+++ README.md
@@ -0,0 +1,11 @@
+Data Structure Headers
+======================
+
+This reposetory holds usefull C headers
+with data structures and some algorithms.
+
+Content Registry
+----------------
+
+- `types.h` - Java/Kotlin style primitive typedefs.
+
---
+++ types.h
@@ -0,0 +1,53 @@
+#ifndef _DSH_TYPES_H
+#define _DSH_TYPES_H
+
+/* Java/Kotlin style primitive typedefs */
+
+#include <stddef.h>
+#include <limits.h>
+
+#if INT_MAX != 0x7FFFFFFFL
+#error Int is expected to be 32-bit.
+#endif
+
+typedef signed char Byte;
+typedef signed short Short;
+typedef signed int Int;
+#if LONG_MAX > INT_MAX || !defined(LLONG_MAX)
+typedef signed long Long;
+#else
+typedef signed long long Long;
+#endif
+
+typedef unsigned char UByte;
+typedef unsigned short UShort;
+typedef unsigned int UInt;
+#if LONG_MAX > INT_MAX || !defined(LLONG_MAX)
+typedef unsigned long ULong;
+#else
+typedef unsigned long long ULong;
+#endif
+
+typedef size_t USize;
+typedef ptrdiff_t SSize, PtrDiff;
+
+typedef int Bool;
+
+#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
+typedef char Boolean;
+#elif __STDC_VERSION__ < 202410L
+typedef _Bool Boolean;
+#else
+typedef bool Boolean;
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#endif
+
