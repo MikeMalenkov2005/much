1383ebc5d5cef81385be96d42afef5d61534dc78
MikeMalenkov2005 <mike.malenkov@gmail.com>
2025-11-21 22:54:58 +0300

    Added utility and sized array macros.

---
+++ array.h
@@ -0,0 +1,18 @@
+#ifndef _DSH_ARRAY_H
+#define _DSH_ARRAY_H
+
+#include "types.h"
+#include <stdlib.h>
+
+#define array_length(array) \
+  (((const USize*)((const Byte*)(array) - 16))[0])
+
+#define array_create(T, length) \
+  ((T*)((Byte*)&(*(USize*)calloc( \
+        1, (length) * sizeof(T) + 16) = (length)) + 16))
+
+#define array_delete(array) \
+  (free((Byte*)(array) - 16))
+
+#endif
+
---
+++ utils.h
@@ -0,0 +1,35 @@
+#ifndef _DSH_UTILS_H
+#define _DSH_UTILS_H
+
+#define STMT(S) do { S } while (0);
+
+#define _STRING(S) #S
+#define STRING(S) _STRING(S)
+
+#define _GLUE(A, B) A ## B
+#define GLUE(A, B) _GLUE(A, B)
+
+#define ARRLEN(array) (sizeof(array) / sizeof(*array))
+
+#define PTR2INT(ptr)  ((char*)(ptr) - (char*)0)
+#define INT2PTR(val)  ((void*)((char*)0 + (val)))
+
+#define MAX(A, B) ((A) < (B) ? (B) : (A))
+#define MIN(A, B) ((A) > (B) ? (B) : (A))
+#define ABS(X) MAX((X), -(X))
+
+#define MEMBER(T, M)  (((T*)0)->M)
+#define OFFSET(T, M)  PTR2INT(&MEMBER(T, M))
+
+#ifdef __cplusplus
+#define CAPI_BEGIN  extern "C" {
+#define CAPI_END    }
+#define CAPI        extern "C"
+#else
+#define CAPI_BEGIN
+#define CAPI_END
+#define CAPI
+#endif
+
+#endif
+
